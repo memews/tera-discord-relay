ef91a5851f743c29ece859c36aaa2aa9b3e7881e
memews <chaos_21@live.com>
2018-06-11 12:22:49 +0800

    Centralized JS and Config

--- discord/index.js
+++ discord/index.js
@@ -1,51 +1,66 @@
 // config
 const fs = require('fs');
-const fn = process.argv[2];
-if (fn == null) {
-  console.error('please specify config file');
-  process.exit(1);
+var bot,Discord,IpcModule,ipc,app,botName,gchatChannel;
+
+var healthStatus = false;
+
+function start(config) {
+	// set up bot
+	const Discord = require('discord.js');
+	const bot = new Discord.Client({
+	  fetchAllMembers: true,
+	  sync: true,
+	});
+
+	bot.on('ready', () => {
+	  console.log('connected as %s (%s)', bot.user.username, bot.user.id);
+	  healthStatus=true
+	  bot.user.setActivity({game: {name: "TERA", type: 0}});
+	  gchatChannel=bot.guilds.get(config['server-id']).channels.get(config.channels['gchat']);
+	});
+
+	bot.on('warn', (warn) => {
+	  console.warn(warn);
+	});
+
+	bot.on('disconnect', () => {
+	  console.log('disconnected');
+	  healthStatus=false;
+	  process.exit();
+	});
+
+	// set up ipc
+	const IpcModule = require('./lib/ipc');
+	const ipc = new IpcModule(config['socket-name']);
+
+	// set up app
+	const app = { bot, ipc };
+
+	console.log('loading submodules...');
+	for (let name of ['gchat', 'entry']) {
+	  const submodule = require('./lib/' + name);
+	  app[submodule] = new submodule(app, config);
+	  if ( name == 'gchat' ) {
+		gchatModule = submodule;
+	  }
+	  console.log('- loaded %s', name);
+	}
+
+	// connect
+	console.log('connecting...');
+	bot.login(config['token']).catch((reason) => {
+	  console.error('failed to login:', reason);
+	  healthStatus=false;
+	  process.exit();
+	});
 }
 
-const config = JSON.parse(fs.readFileSync(fn, 'utf8'));
-
-// set up bot
-const Discord = require('discord.js');
-const bot = new Discord.Client({
-  fetchAllMembers: true,
-  sync: true,
-});
-
-bot.on('ready', () => {
-  console.log('connected as %s (%s)', bot.user.username, bot.user.id);
-  bot.user.setActivity({game: {name: "TERA", type: 0}});
-});
-
-bot.on('warn', (warn) => {
-  console.warn(warn);
-});
-
-bot.on('disconnect', () => {
-  console.log('disconnected');
-  process.exit();
-});
-
-// set up ipc
-const IpcModule = require('./lib/ipc');
-const ipc = new IpcModule(config['socket-name']);
-
-// set up app
-const app = { bot, ipc };
-
-console.log('loading submodules...');
-for (let name of ['gchat', 'entry']) {
-  const submodule = require('./lib/' + name);
-  app[submodule] = new submodule(app, config);
-  console.log('- loaded %s', name);
+function healthCheck() {
+	return healthStatus;
 }
 
-// connect
-console.log('connecting...');
-bot.login(config['token']).catch((reason) => {
-  console.error('failed to login:', reason);
-  process.exit();
-});
+module.exports = {
+	start:start,
+	healthCheck:healthCheck,
+	isRelog:isRelog
+}
--- tera/index.js
+++ tera/index.js
@@ -1,166 +1,174 @@
 // config
 const fs = require('fs');
-const fn = process.argv[2];
+var healthStatus = false;
 
-if (fn == null) {
-  console.error('please specify config file');
-  process.exit(1);
+function healthCheck() {
+	return healthStatus;
+
+function start(config) {
+	// client
+	const webClient = require('tera-auth-ticket');
+	const { Connection, FakeClient } = require('tera-proxy-game');
+
+	const describe = (() => {
+	  const races = ['Human', 'High Elf', 'Aman', 'Castanic', 'Popori', 'Baraka'];
+	  const genders = ['Male', 'Female'];
+
+	  const classes = [
+		'Warrior', 'Lancer', 'Slayer', 'Berserker', 'Sorcerer', 'Archer',
+		'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie',
+	  ];
+
+	  return function describe(character) {
+		let description = '';
+
+		// race & gender
+		const race = races[character.race] || '?';
+		const gender = genders[character.gender] || '?';
+
+		if (character.race < 4) {
+		  description += `${race} ${gender}`;
+		} else {
+		  if (character.race === 4 && character.gender === 1) {
+			description += 'Elin';
+		  } else {
+			description += race;
+		  }
+		}
+
+		// class
+		description += ' ' + (classes[character['class']] || '?') + ' / ';
+
+		// level
+		description += character.level;
+
+		// return
+		return description;
+	  };
+	})();
+
+	// main
+	const web = new webClient(config.email, config.pass);
+	web.getLogin((err, data) => {
+	  if (err) return;
+
+	  const connection = new Connection();
+	  const client = new FakeClient(connection);
+	  const srvConn = connection.connect(client, { host: config.host, port: config.port });
+
+	  let closed = false;
+
+	  function closeClient() {
+		if (closed) return;
+
+		closed = true;
+		client.close();
+
+		setImmediate(() => {
+		  process.exit();
+		});
+	  }
+
+	  connection.dispatch.setProtocolVersion(config.protocolVersion);
+	  
+	  // set up core bot features
+	  connection.dispatch.load('<core>', function coreModule(dispatch) {
+		// `connect` handler
+		client.on('connect', () => {
+		  // authorization
+		  dispatch.toServer('C_LOGIN_ARBITER', 1, {
+			unk1: 0,
+			unk2: 0,
+			unk3: 2,
+			patchVersion: config.patchVersion,
+			name: data.name,
+			ticket: new Buffer(data.ticket),
+		  });
+		});
+
+		// get character list
+		dispatch.hook('S_LOGIN_ACCOUNT_INFO', 1, () => {
+		  dispatch.toServer('C_GET_USER_LIST', 1);
+		});
+
+		dispatch.hook('S_GET_USER_LIST', 1, (event) => {
+		  // parse character list
+		  const characters = new Map();
+		  for (const character of event.characters) {
+			characters.set(character.name.toLowerCase(), {
+			  id: character.id,
+			  description: `${character.name} [${describe(character)}]`,
+			});
+		  }
+
+		  // find matching character
+		  const character = characters.get(config.character.toLowerCase());
+		  if (!character) {
+			console.error(`[client] no character "${config.character}"`);
+			console.error('[client] character list:');
+			for (const char of characters.values()) {
+			  console.error(`- ${char.description} (id: ${char.id})`);
+			}
+		  } else {
+			console.log(`[client] logging onto ${character.description} (id: ${character.id})`);
+			healthStatus=true;
+			dispatch.toServer('C_SELECT_USER', 1, {
+			  id: character.id,
+			  unk: 0,
+			});
+		  }
+		});
+
+		// login sequence
+		dispatch.hook('S_LOAD_TOPO', 1, () => {
+		  dispatch.toServer('C_LOAD_TOPO_FIN', 1);
+		});
+
+		// ping-pong
+		dispatch.hook('S_PING', 1, () => {
+		  dispatch.toServer('C_PONG', 1);
+		});
+
+		// terminate when connection ends
+		client.on('close', () => {
+		  healthStatus=false;
+		  closeClient();
+		});
+	  });
+
+	  // load modules
+	  for (const moduleName in config.modules) {
+		const moduleConfig = config.modules[moduleName];
+		connection.dispatch.load('./app/' + moduleName, module, moduleConfig);
+	  }
+
+	  // logging
+	  srvConn.setTimeout(30 * 1000);
+
+	  srvConn.on('connect', () => {
+		console.log(`<connected to ${srvConn.remoteAddress}:${srvConn.remotePort}>`);
+		healthStatus=true;
+	  });
+
+	  srvConn.on('timeout', () => {
+		console.log('<timeout>');
+		healthStatus=false;
+		closeClient();
+	  });
+
+	  srvConn.on('close', () => {
+		console.log('<disconnected>');
+		healthStatus=false;
+		process.exit();
+	  });
+
+	  srvConn.on('error', (err) => {
+		console.warn(err);
+	  });
+	});
 }
 
-const config = JSON.parse(fs.readFileSync(fn, 'utf8'));
-
-// client
-const webClient = require('tera-auth-ticket');
-const { Connection, FakeClient } = require('tera-proxy-game');
-
-const describe = (() => {
-  const races = ['Human', 'High Elf', 'Aman', 'Castanic', 'Popori', 'Baraka'];
-  const genders = ['Male', 'Female'];
-
-  const classes = [
-    'Warrior', 'Lancer', 'Slayer', 'Berserker', 'Sorcerer', 'Archer',
-    'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie',
-  ];
-
-  return function describe(character) {
-    let description = '';
-
-    // race & gender
-    const race = races[character.race] || '?';
-    const gender = genders[character.gender] || '?';
-
-    if (character.race < 4) {
-      description += `${race} ${gender}`;
-    } else {
-      if (character.race === 4 && character.gender === 1) {
-        description += 'Elin';
-      } else {
-        description += race;
-      }
-    }
-
-    // class
-    description += ' ' + (classes[character['class']] || '?') + ' / ';
-
-    // level
-    description += character.level;
-
-    // return
-    return description;
-  };
-})();
-
-// main
-const web = new webClient(config.email, config.pass);
-web.getLogin((err, data) => {
-  if (err) return;
-
-  const connection = new Connection();
-  const client = new FakeClient(connection);
-  const srvConn = connection.connect(client, { host: config.host, port: config.port });
-
-  let closed = false;
-
-  function closeClient() {
-    if (closed) return;
-
-    closed = true;
-    client.close();
-
-    setImmediate(() => {
-      process.exit();
-    });
-  }
-
-  connection.dispatch.setProtocolVersion(config.protocolVersion);
-  
-  // set up core bot features
-  connection.dispatch.load('<core>', function coreModule(dispatch) {
-    // `connect` handler
-    client.on('connect', () => {
-      // authorization
-      dispatch.toServer('C_LOGIN_ARBITER', 1, {
-        unk1: 0,
-        unk2: 0,
-        unk3: 2,
-        patchVersion: config.patchVersion,
-        name: data.name,
-        ticket: new Buffer(data.ticket),
-      });
-    });
-
-    // get character list
-    dispatch.hook('S_LOGIN_ACCOUNT_INFO', 1, () => {
-      dispatch.toServer('C_GET_USER_LIST', 1);
-    });
-
-    dispatch.hook('S_GET_USER_LIST', 1, (event) => {
-      // parse character list
-      const characters = new Map();
-      for (const character of event.characters) {
-        characters.set(character.name.toLowerCase(), {
-          id: character.id,
-          description: `${character.name} [${describe(character)}]`,
-        });
-      }
-
-      // find matching character
-      const character = characters.get(config.character.toLowerCase());
-      if (!character) {
-        console.error(`[client] no character "${config.character}"`);
-        console.error('[client] character list:');
-        for (const char of characters.values()) {
-          console.error(`- ${char.description} (id: ${char.id})`);
-        }
-      } else {
-        console.log(`[client] logging onto ${character.description} (id: ${character.id})`);
-        dispatch.toServer('C_SELECT_USER', 1, {
-          id: character.id,
-          unk: 0,
-        });
-      }
-    });
-
-    // login sequence
-    dispatch.hook('S_LOAD_TOPO', 1, () => {
-      dispatch.toServer('C_LOAD_TOPO_FIN', 1);
-    });
-
-    // ping-pong
-    dispatch.hook('S_PING', 1, () => {
-      dispatch.toServer('C_PONG', 1);
-    });
-
-    // terminate when connection ends
-    client.on('close', () => {
-      closeClient();
-    });
-  });
-
-  // load modules
-  for (const moduleName in config.modules) {
-    const moduleConfig = config.modules[moduleName];
-    connection.dispatch.load('./app/' + moduleName, module, moduleConfig);
-  }
-
-  // logging
-  srvConn.setTimeout(30 * 1000);
-
-  srvConn.on('connect', () => {
-    console.log(`<connected to ${srvConn.remoteAddress}:${srvConn.remotePort}>`);
-  });
-
-  srvConn.on('timeout', () => {
-    console.log('<timeout>');
-    closeClient();
-  });
-
-  srvConn.on('close', () => {
-    console.log('<disconnected>');
-    process.exit();
-  });
-
-  srvConn.on('error', (err) => {
-    console.warn(err);
-  });
-});
+module.exports = {
+	start:start,
+	healthCheck:healthCheck
+}
